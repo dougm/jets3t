44311014548f8ab77ece86d2adcfc27c3456433a
jmurty <jmurty>
2006-09-08 05:38:54 +0000

    Scripts for the distribution

---
+++ scripts/cockpit.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM Include libraries in classpath
+set CP=jets3t.jar
+set CP=%CP%;cockpit.jar
+set CP=%CP%;libs/commons-logging/commons-logging-1.1.jar
+set CP=%CP%;libs/commons-codec/commons-codec-1.3.jar
+set CP=%CP%;libs/commons-httpclient/commons-httpclient-3.0.1.jar
+set CP=%CP%;libs/misc/BareBonesBrowserLaunch.jar
+
+java -classpath %CP% org.jets3t.apps.cockpit.Cockpit
---
+++ scripts/cockpit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Include libraries in classpath
+CP=jets3t.jar
+CP=$CP:cockpit.jar
+CP=$CP:libs/commons-logging/commons-logging-1.1.jar
+CP=$CP:libs/commons-codec/commons-codec-1.3.jar
+CP=$CP:libs/commons-httpclient/commons-httpclient-3.0.1.jar
+CP=$CP:libs/misc/BareBonesBrowserLaunch.jar
+
+java -classpath $CP org.jets3t.apps.cockpit.Cockpit
