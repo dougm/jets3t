a1e457ac01841a53d8b6909eed2563706049807c
jmurty <jmurty>
2006-11-17 23:18:23 +0000

    Fixed script to group quoted arguments together rather than separating them.Issue number:  23 Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- scripts/synchronize.sh
+++ scripts/synchronize.sh
@@ -7,4 +7,4 @@ CP=$CP:libs/commons-logging/commons-logging-1.1.jar
 CP=$CP:libs/commons-codec/commons-codec-1.3.jar
 CP=$CP:libs/commons-httpclient/commons-httpclient-3.0.1.jar
 
-java -classpath $CP org.jets3t.apps.synchronize.Synchronize $@
+java -classpath $CP org.jets3t.apps.synchronize.Synchronize "$@"
