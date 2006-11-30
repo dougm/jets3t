b7d45a159837e5163074896ef937743c711497af
jmurty <jmurty>
2006-11-30 12:00:57 +0000

    Changes:- Added missing license comment header- Removed non-vital bucket deletes, to work around S3 fussiness concerning this operation

--- src/org/jets3t/tests/HttpClientRestS3ServiceTest.java
+++ src/org/jets3t/tests/HttpClientRestS3ServiceTest.java
@@ -1,3 +1,21 @@
+/*
+ * jets3t : Java Extra-Tasty S3 Toolkit (for Amazon S3 online storage service)
+ * This is a java.net project, see https://jets3t.dev.java.net/
+ * 
+ * Copyright 2006 James Murty
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.jets3t.tests;
 
 import java.io.IOException;
@@ -26,7 +44,7 @@ public class HttpClientRestS3ServiceTest extends BaseS3ServiceTest {
         S3Service s3Service = getS3Service(awsCredentials);
         
         // Create test bucket.
-        String bucketName = awsCredentials.getAccessKey() + ".HttpClientRestS3ServiceTest";
+        String bucketName = awsCredentials.getAccessKey() + ".jets3t_TestCases";
         S3Bucket bucket = s3Service.createBucket(bucketName);
         
         // Try to create REST canned public object.        
@@ -43,7 +61,7 @@ public class HttpClientRestS3ServiceTest extends BaseS3ServiceTest {
                     200, ((HttpURLConnection)url.openConnection()).getResponseCode());
         } finally {
             s3Service.deleteObject(bucket, object.getKey());
-            s3Service.deleteBucket(bucket.getName());
+//            s3Service.deleteBucket(bucket.getName());
         }
     }
 
