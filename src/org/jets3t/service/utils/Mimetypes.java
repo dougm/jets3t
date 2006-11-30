e6178f091a953d65df88e6a47f481c29e15b7022
jmurty <jmurty>
2006-11-30 11:41:32 +0000

    Mime type settings are now read from a file (mime.types) matching the formatting of the Apache HTTPD server mime.types file.Issue number:  8 Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- src/org/jets3t/service/utils/Mimetypes.java
+++ src/org/jets3t/service/utils/Mimetypes.java
@@ -18,18 +18,36 @@
  */
 package org.jets3t.service.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
- * Utility class to contain known Mimetypes, and determine the mimetypes of files based on the file's
+ * Utility class to contain known Mimetypes, and determine the mimetype of files based on the file's
  * extension.
- *
- * TODO There is much work to do here.
+ * <p>
+ * This class is statically loaded with mime types from the file <code>mime.types</code> if this
+ * file is available at the root of the classpath. The mime.types file format, and most of the
+ * content, is taken from the Apache HTTP server's mime.types file.
+ * <p>
+ * The format for mime type setting documents is: 
+ * <code>mimetype <Space | Tab>+ extension (<Space|Tab>+ extension)*</code>.
+ * Any blank lines in the file are ignored, as are lines starting with <code>#</code> which are
+ * considered comments. Lines that have a mimetype but no associated extensions are also ignored.
  * 
  * @author James Murty
  */
 public class Mimetypes {
-    // Public mimetypes.
+    private static final Log log = LogFactory.getLog(Mimetypes.class);
+    
+    // Mimetypes used frequently in jets3t.
     public static String MIMETYPE_XML = "application/xml";
     public static String MIMETYPE_HTML = "text/html";
     public static String MIMETYPE_OCTET_STREAM = "application/octet-stream";
@@ -39,24 +57,113 @@ public class Mimetypes {
     public static String MIMETYPE_JETS3T_DIRECTORY = "application/x-directory";
     
     /**
+     * Map that stores file extensions as keys, and the corresponding mimetype as values.
+     */
+    private static HashMap extensionToMimetypeMap = new HashMap();
+    
+    /**
+     * Loads mime type settings from the file 'mime.types' in the classpath, if it's available.
+     */
+    static {
+        InputStream mimetypesFile = extensionToMimetypeMap.getClass().getResourceAsStream("/mime.types");
+        if (mimetypesFile != null) {
+            log.debug("Loading mime types from file in the classpath: mime.types");
+            try {
+                loadAndReplaceMimetypes(mimetypesFile);
+            } catch (IOException e) {
+                log.error("Failed to load mime types from file in the classpath: mime.types", e); 
+            }
+        }
+    }
+
+    /**
+     * Reads and stores the mime type setting corresponding to a file extension, by reading
+     * text from an InputStream. If a mime type setting already exists when this method is run, 
+     * the mime type value is replaced with the newer one.
+     *  
+     * @param is
+     * 
+     * @throws IOException
+     */
+    public static void loadAndReplaceMimetypes(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line =  null;
+        
+        while ((line = br.readLine()) != null) {
+            line = line.trim();
+
+            if (line.startsWith("#") || line.length() == 0) {
+                // Ignore comments and empty lines.
+            } else {
+                StringTokenizer st = new StringTokenizer(line, " \t");
+                if (st.countTokens() > 1) {
+                    String mimetype = st.nextToken();
+                    while (st.hasMoreTokens()) {
+                        String extension = st.nextToken();
+                        extensionToMimetypeMap.put(extension, mimetype);
+                        log.debug("Setting mime type for extension '" + extension + "' to '" + mimetype + "'");
+                    }
+                } else {
+                    log.debug("Ignoring mimetype with no associated file extensions: '" + line + "'");                    
+                }
+            }
+        }
+    }
+
+    /**
+     * Determines the mimetype of a file by looking up the file's extension in an internal listing
+     * to find the corresponding mime type. If the file has no extension, or the extension is not
+     * available in the listing contained in this class, the default mimetype 
+     * <code>application/octet-stream</code> is returned. 
+     * <p>
+     * A file extension is one or more characters that occur after the last period (.) in the file's name.
+     * If a file has no extension, 
+     * Guesses the mimetype of file data based on the file's extension. 
+     *  
+     * @param fileName
+     * the name of the file whose extension may match a known mimetype.
+     * 
+     * @return
+     * the file's mimetype based on its extension, or a default value of 
+     * <code>application/octet-stream</code> if a mime type value cannot be found.
+     */
+    public static String getMimetype(String fileName) {
+        int lastPeriodIndex = fileName.lastIndexOf(".");
+        if (lastPeriodIndex > 0 && lastPeriodIndex + 1 < fileName.length()) {
+            String ext = fileName.substring(lastPeriodIndex + 1);
+            if (extensionToMimetypeMap.keySet().contains(ext)) {
+                String mimetype = (String) extensionToMimetypeMap.get(ext);
+                log.debug("Recognised extension '" + ext + "', mimetype is: '" + mimetype + "'");                
+                return mimetype;
+            } else {
+                log.debug("Extension '" + ext + "' is unrecognized in mime type listing"
+                    + ", using default mime type: '" + MIMETYPE_OCTET_STREAM + "'");                
+            }
+        } else {
+            log.debug("File name has no extension, mime type cannot be recognised for: " + fileName);
+        }
+        return MIMETYPE_OCTET_STREAM;
+    }
+    
+    /**
+     * Determines the mimetype of a file by looking up the file's extension in an internal listing
+     * to find the corresponding mime type. If the file has no extension, or the extension is not
+     * available in the listing contained in this class, the default mimetype 
+     * <code>application/octet-stream</code> is returned. 
+     * <p>
+     * A file extension is one or more characters that occur after the last period (.) in the file's name.
+     * If a file has no extension, 
      * Guesses the mimetype of file data based on the file's extension. 
      *  
      * @param file
+     * the file whose extension may match a known mimetype.
+     * 
      * @return
+     * the file's mimetype based on its extension, or a default value of 
+     * <code>application/octet-stream</code> if a mime type value cannot be found.
      */
     public static String getMimetype(File file) {
-        if (file.getName().endsWith(".html") || file.getName().endsWith(".htm")) {
-            return MIMETYPE_HTML;
-        } 
-        else if (file.getName().endsWith(".xml")) {
-            return MIMETYPE_XML;
-        }
-        else if (file.getName().endsWith(".gz")) {
-            return MIMETYPE_GZIP;
-        }
-        else {
-            return MIMETYPE_OCTET_STREAM;
-        }
+       return getMimetype(file.getName()); 
     }
-    
+        
 }
