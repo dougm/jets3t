5887a02f1e9aff42e72448af38186ae1c74f0906
jmurty <jmurty>
2006-11-30 11:35:32 +0000

    Added missing header licensing comments

--- src/org/jets3t/samples/CodeSamples.java
+++ src/org/jets3t/samples/CodeSamples.java
@@ -1,3 +1,21 @@
+/*
+ * jets3t : Java Extra-Tasty S3 Toolkit (for Amazon S3 online storage service)
+ * This is a java.net project, see https://jets3t.dev.java.net/
+ * 
+ * Copyright 2006 James Murty
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.jets3t.samples;
 
 import java.io.BufferedReader;
@@ -477,10 +495,10 @@ public class CodeSamples {
         cal.add(Calendar.MINUTE, 5);
         Date expiryDate = cal.getTime();
         
-        // Create a signed HTTP URL valid for 5 minutes.
+        // Create a signed HTTP GET URL valid for 5 minutes.
         boolean isHttpsUrl = true; // Choose whether URL is HTTP or HTTPS.
-        String url = S3Service.createSignedUrl(
-            privateBucket.getName(), privateObject.getKey(), 
+        String url = S3Service.createSignedUrl("GET", privateBucket.getName(), 
+            privateObject.getKey(), null,  
             awsCredentials, expiryDate, isHttpsUrl);
         System.out.println("Signed URL: " + url);
     }
