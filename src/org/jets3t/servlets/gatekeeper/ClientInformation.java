c4eda80753e2b5ff03da44589ba5963c25e4c848
jmurty <jmurty>
2006-12-08 13:57:10 +0000

    Implemented this class, which now actually stores some client information.

--- src/org/jets3t/servlets/gatekeeper/ClientInformation.java
+++ src/org/jets3t/servlets/gatekeeper/ClientInformation.java
@@ -1,5 +1,60 @@
 package org.jets3t.servlets.gatekeeper;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Stores information about an HTTP client that submitted a request to the Gatekeeper.
+ * <p>
+ * The information available about a client will depend on the server and client configuration,
+ * such as whether the client is identified with an existing HttpSession or Principal. It must
+ * be assumed that much of the information stored in this class will have a null value in many
+ * cases.
+ * 
+ * @author James Murty
+ */
 public class ClientInformation {
+    private String remoteAddress = null;
+    private String remoteHost = null;
+    private String remoteUser = null;
+    private int remotePort = -1;
+    private HttpSession session = null;
+    private Principal userPrincipal = null;
+    
+    public ClientInformation(String remoteAddress, String remoteHost, String remoteUser,
+        int remotePort, HttpSession session, Principal userPrincipal) 
+    {
+        this.remoteAddress = remoteAddress;
+        this.remoteHost = remoteHost;
+        this.remoteUser = remoteUser;
+        this.remotePort = remotePort;
+        this.session = session;
+        this.userPrincipal = userPrincipal;
+    }
+
+    public String getRemoteAddress() {
+        return remoteAddress;
+    }
+
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    public int getRemotePort() {
+        return remotePort;
+    }
+
+    public String getRemoteUser() {
+        return remoteUser;
+    }
+
+    public HttpSession getSession() {
+        return session;
+    }
+
+    public Principal getUserPrincipal() {
+        return userPrincipal;
+    }
 
 }
