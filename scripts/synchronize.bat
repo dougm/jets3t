c367c908279e53a379e973e92170b874e747ef90
jmurty <jmurty>
2006-12-18 11:44:36 +0000

    Fixed mistaken delimiter in classpath, ':' to ';'

--- scripts/synchronize.bat
+++ scripts/synchronize.bat
@@ -9,6 +9,6 @@ set CP=%CP%;jars/synchronize-0.5.0.jar
 set CP=%CP%;libs/commons-logging/commons-logging-1.1.jar
 set CP=%CP%;libs/commons-codec/commons-codec-1.3.jar
 set CP=%CP%;libs/commons-httpclient/commons-httpclient-3.0.1.jar
-set CP=%CP%:libs/logging-log4j/log4j-1.2.13.jar
+set CP=%CP%;libs/logging-log4j/log4j-1.2.13.jar
 
 java -classpath %CP% org.jets3t.apps.synchronize.Synchronize %*
--- scripts/uploader.bat
+++ scripts/uploader.bat
@@ -10,6 +10,6 @@ set CP=%CP%;libs/commons-logging/commons-logging-1.1.jar
 set CP=%CP%;libs/commons-codec/commons-codec-1.3.jar
 set CP=%CP%;libs/commons-httpclient/commons-httpclient-3.0.1.jar
 set CP=%CP%;libs/misc/BareBonesBrowserLaunch.jar
-set CP=%CP%:libs/logging-log4j/log4j-1.2.13.jar
+set CP=%CP%;libs/logging-log4j/log4j-1.2.13.jar
 
 start javaw -classpath %CP% org.jets3t.apps.uploader.Uploader
