c0f19a1d7f3bbae38c5e65c4779bc7e1065a7a7a
jmurty <jmurty>
2007-01-16 12:11:30 +0000

    Fixed missing Apache Xerces libraries, which are required for XML summary support in the Uploader

--- scripts/uploader.sh
+++ scripts/uploader.sh
@@ -56,6 +56,8 @@ CP=$CP:$JETS3T_HOME/jars/uploader-0.5.0.jar
 CP=$CP:$JETS3T_HOME/libs/commons-logging/commons-logging-1.1.jar
 CP=$CP:$JETS3T_HOME/libs/commons-codec/commons-codec-1.3.jar
 CP=$CP:$JETS3T_HOME/libs/commons-httpclient/commons-httpclient-3.0.1.jar
+CP=$CP:$JETS3T_HOME/libs/xerces/xercesImpl.jar
+CP=$CP:$JETS3T_HOME/libs/xerces/xmlParserAPIs.jar
 CP=$CP:$JETS3T_HOME/libs/misc/BareBonesBrowserLaunch.jar
 CP=$CP:$JETS3T_HOME/libs/logging-log4j/log4j-1.2.13.jar
 
