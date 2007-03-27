2775de3c4e0de0489a413b063c644f22e43dfcdd
jmurty <jmurty>
2007-03-27 11:07:52 +0000

    Updated references for new log4j library

--- scripts/uploader.sh
+++ scripts/uploader.sh
@@ -57,6 +57,6 @@ CP=$CP:$JETS3T_HOME/libs/commons-logging/commons-logging-1.1.jar
 CP=$CP:$JETS3T_HOME/libs/commons-codec/commons-codec-1.3.jar
 CP=$CP:$JETS3T_HOME/libs/commons-httpclient/commons-httpclient-3.0.1.jar
 CP=$CP:$JETS3T_HOME/libs/misc/BareBonesBrowserLaunch.jar
-CP=$CP:$JETS3T_HOME/libs/logging-log4j/log4j-1.2.13.jar
+CP=$CP:$JETS3T_HOME/libs/logging-log4j/log4j-1.2.14.jar
 
 $EXEC -classpath $CP org.jets3t.apps.uploader.Uploader
