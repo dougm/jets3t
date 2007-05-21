d10ae53c622009e0a4cce266e56d748bb809056b
jmurty <jmurty>
2007-05-21 04:15:01 +0000

    Updated to use new BytesProgressWatcher class

--- src/org/jets3t/service/io/ProgressMonitoredInputStream.java
+++ src/org/jets3t/service/io/ProgressMonitoredInputStream.java
@@ -23,57 +23,34 @@ import java.io.InputStream;
 
 /**
  * Input stream wrapper that tracks the number of bytes that have been read through the stream.
- * When data is read through this stream the count of bytes is increased, and at a set minimum 
- * interval (eg after at least 1024 bytes) a {@link BytesTransferredWatcher} implementation
- * is notified of the count of bytes read since the last notification.  
+ * When data is read through this stream the count of bytes is increased and the associated
+ * {@link BytesTransferredWatcher} object is notified of the count of bytes read.  
  *  
  * @author James Murty
  */
 public class ProgressMonitoredInputStream extends InputStream implements InputStreamWrapper {
     private InputStream inputStream = null;
-    private BytesTransferredWatcher bytesTransferredWatcher = null;
-    private long minimumBytesBeforeNotification = 1024;
-    private long bytesTransferredTotal = 0;
-    private long bytesTransferredLastUpdate = 0;
-
+    private BytesProgressWatcher progressWatcher = null;
+ 
     /**
      * Construts the input stream around an underlying stream and sends notification messages
-     * to a watcher at intervals.
+     * to a progress watcher when bytes are read from the stream.
      * 
      * @param inputStream
      *        the input stream to wrap, whose byte transfer count will be monitored.
-     * @param bytesTransferredWatcher
-     *        a notification watcher
-     * @param minimumBytesBeforeNotification
-     *        the minimum number of bytes that must be transferred before a notification will be triggered
+     * @param progressWatcher
+     *        a watcher object that stores information about the bytes read from a stream, and
+     *        allows calculations to be perfomed using this information.
      */
-    public ProgressMonitoredInputStream(InputStream inputStream, 
-        BytesTransferredWatcher bytesTransferredWatcher, long minimumBytesBeforeNotification) 
-    {
+    public ProgressMonitoredInputStream(InputStream inputStream, BytesProgressWatcher progressWatcher) {
         if (inputStream == null) {
             throw new IllegalArgumentException(
                 "ProgressMonitoredInputStream cannot run with a null InputStream");
         }
         this.inputStream = inputStream;
-        this.bytesTransferredWatcher = bytesTransferredWatcher;
-        this.minimumBytesBeforeNotification = minimumBytesBeforeNotification;
+        this.progressWatcher = progressWatcher;
     }
-
-    /**
-     * Construts the input stream around an underlying stream and sends notification messages
-     * to a watcher at minimum byte intervals of 1024.
-     * 
-     * @param inputStream
-     *        the input stream to wrap, whose byte transfer count will be monitored.
-     * @param bytesTransferredWatcher
-     *        a notification watcher
-     */
-    public ProgressMonitoredInputStream(InputStream inputStream, 
-        BytesTransferredWatcher bytesTransferredWatcher) 
-    {
-        this(inputStream, bytesTransferredWatcher, 1024);
-    }
-
+    
     /**
      * Checks how many bytes have been transferred since the last notification, and sends a notification
      * message if this number exceeds the minimum bytes transferred value.
@@ -81,17 +58,11 @@ public class ProgressMonitoredInputStream extends InputStream implements InputSt
      * @param bytesTransmitted
      */
     public void sendNotificationUpdate(long bytesTransmitted) {
-        bytesTransferredTotal += bytesTransmitted;
-        
-        if (bytesTransferredWatcher != null) {
-            // Notify watcher if more than the minimum number of bytes have been transferred since last
-            // time, or if a negative update value is received (ie if a transmission is being retried)
-            long bytesSinceLastUpdate = bytesTransferredTotal - bytesTransferredLastUpdate;
-            if (bytesSinceLastUpdate > minimumBytesBeforeNotification || bytesSinceLastUpdate < 0) {
-                bytesTransferredWatcher.bytesTransferredUpdate(bytesSinceLastUpdate);
-                bytesTransferredLastUpdate = bytesTransferredTotal;
-            }
-        }
+        progressWatcher.updateBytesTransferred(bytesTransmitted);        
+    }
+    
+    public void resetProgressMonitor() {
+        progressWatcher.resetWatcher();
     }
     
     public int read() throws IOException {
@@ -104,6 +75,14 @@ public class ProgressMonitoredInputStream extends InputStream implements InputSt
     
     public int read(byte[] b, int off, int len) throws IOException {
         int read = inputStream.read(b, off, len);
+        if (read != -1) {
+            sendNotificationUpdate(read);
+        }
+        return read;
+    }
+    
+    public int read(byte[] b) throws IOException {
+        int read = inputStream.read(b);
         if (read != -1) {
             sendNotificationUpdate(read);
         }
