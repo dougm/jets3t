fd0b891889e4e81f6cef3168f1e8e1b25e3e7a0a
jmurty <jmurty>
2007-07-10 01:52:48 +0000

    Dialog is now skinnable

--- src/org/jets3t/gui/ErrorDialog.java
+++ src/org/jets3t/gui/ErrorDialog.java
@@ -1,3 +1,21 @@
+/*
+ * jets3t : Java Extra-Tasty S3 Toolkit (for Amazon S3 online storage service)
+ * This is a java.net project, see https://jets3t.dev.java.net/
+ * 
+ * Copyright 2007 James Murty
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.jets3t.gui;
 
 import java.awt.Frame;
@@ -6,12 +24,19 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jets3t.gui.skins.SkinsFactory;
 import org.jets3t.service.Constants;
 import org.jets3t.service.Jets3tProperties;
 import org.jets3t.service.S3ServiceException;
@@ -22,44 +47,72 @@ import org.jets3t.service.S3ServiceException;
  * @author James Murty
  */
 public class ErrorDialog extends JDialog implements ActionListener {
-    private static final long serialVersionUID = -1913801256028107392L;
+    private static final long serialVersionUID = 6538639968961532471L;
+
+    private static final Log log = LogFactory.getLog(ErrorDialog.class);
 
     private Jets3tProperties jets3tProperties = 
         Jets3tProperties.getInstance(Constants.JETS3T_PROPERTIES_FILENAME);
     
+    private Properties applicationProperties = null;
+
     private HyperlinkActivatedListener hyperlinkListener = null;
+    private SkinsFactory skinsFactory = null;
     
     private final Insets insetsZero = new Insets(0, 0, 0, 0);
     private final Insets insetsDefault = new Insets(3, 5, 3, 5);
 
     
-    private ErrorDialog(Frame ownerFrame, HyperlinkActivatedListener hyperlinkListener) {
+    private ErrorDialog(Frame ownerFrame, HyperlinkActivatedListener hyperlinkListener, 
+    		Properties applicationProperties) 
+    {
         super(ownerFrame, "Error Message", true);
         this.hyperlinkListener = hyperlinkListener;
+        this.applicationProperties = applicationProperties;
     }
     
-    private ErrorDialog(JDialog ownerDialog, HyperlinkActivatedListener hyperlinkListener) {
+    private ErrorDialog(JDialog ownerDialog, HyperlinkActivatedListener hyperlinkListener,
+    		Properties applicationProperties) 
+    {
         super(ownerDialog, "Error Message", true);
         this.hyperlinkListener = hyperlinkListener;
+        this.applicationProperties = applicationProperties;
     }
     
     /**
      * Initialises all GUI elements.
      */
     private void initGui(String message, String details) {
+        // Initialise skins factory. 
+        skinsFactory = SkinsFactory.getInstance(applicationProperties); 
+        
+        // Set Skinned Look and Feel.
+        LookAndFeel lookAndFeel = skinsFactory.createSkinnedMetalTheme("SkinnedLookAndFeel");        
+        try {
+            UIManager.setLookAndFeel(lookAndFeel);
+        } catch (UnsupportedLookAndFeelException e) {
+            log.error("Unable to set skinned LookAndFeel", e);
+        }    	
+    	
         this.setResizable(false);
         this.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
 
-        JHtmlLabel messageLabel = new JHtmlLabel(message, hyperlinkListener);
+        JHtmlLabel messageLabel = skinsFactory
+        	.createSkinnedJHtmlLabel("ErrorMessageLabel", hyperlinkListener);
+        messageLabel.setText(message);
         messageLabel.setHorizontalAlignment(JLabel.CENTER);
-        JHtmlLabel detailsLabel = new JHtmlLabel(details, hyperlinkListener);
+        JHtmlLabel detailsLabel = skinsFactory
+    		.createSkinnedJHtmlLabel("ErrorDetailsLabel", hyperlinkListener);
+        detailsLabel.setText(details);
 
-        JButton okButton = new JButton("OK");
+        JButton okButton = skinsFactory.createSkinnedJButton("ErrorOkButton"); 
         okButton.setName("OK");
+        okButton.setText("OK");
         okButton.addActionListener(this);
         this.getRootPane().setDefaultButton(okButton);
 
-        JPanel buttonsPanel = new JPanel(new GridBagLayout());
+        JPanel buttonsPanel = skinsFactory.createSkinnedJPanel("ErrorButtonsPanel");
+        buttonsPanel.setLayout(new GridBagLayout());
         buttonsPanel.add(okButton, new GridBagConstraints(0, 0, 
             1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, insetsZero, 0, 0));
         
@@ -162,9 +215,9 @@ public class ErrorDialog extends JDialog implements ActionListener {
      * the underlying exception that caused the error. 
      */
     public static void showDialog(Frame ownerFrame, HyperlinkActivatedListener hyperlinkListener,
-        String message, Throwable throwable) 
+    		Properties applicationProperties, String message, Throwable throwable) 
     {
-        ErrorDialog dialog = new ErrorDialog(ownerFrame, hyperlinkListener);
+        ErrorDialog dialog = new ErrorDialog(ownerFrame, hyperlinkListener, applicationProperties);
         dialog.initGui(message, dialog.buildDetailedText(throwable));
         dialog.setVisible(true);
         dialog.dispose();
@@ -187,12 +240,56 @@ public class ErrorDialog extends JDialog implements ActionListener {
      * the underlying exception that caused the error. 
      */
     public static void showDialog(JDialog ownerDialog, HyperlinkActivatedListener hyperlinkListener,
-        String message, Throwable throwable) 
+    		Properties applicationProperties, String message, Throwable throwable) 
     {
-        ErrorDialog dialog = new ErrorDialog(ownerDialog, hyperlinkListener);
+        ErrorDialog dialog = new ErrorDialog(ownerDialog, hyperlinkListener, applicationProperties);
         dialog.initGui(message, dialog.buildDetailedText(throwable));
         dialog.setVisible(true);
         dialog.dispose();
+    }
+    
+    /**
+     * Shows the error dialog and waits for the user to acknowledge the dialog.
+     * <p>
+     * If the JetS3t property <tt>gui.verboseErrorDialog</tt> is set to true, this dialog will 
+     * display detailed information about the root cause of the error (the throwable, if provided)
+     * </p>
+     *
+     * @param ownerFrame
+     * the frame that will own the dialog
+     * @param hyperlinkListener
+     * the listener that will act on any hyperlink events triggered by the user clicking on HTTP links.
+     * @param message
+     * a general error message, which should ideally be somewhat user-friendly.
+     * @param throwable
+     * the underlying exception that caused the error. 
+     */
+    public static void showDialog(Frame ownerFrame, HyperlinkActivatedListener hyperlinkListener,
+    		String message, Throwable throwable) 
+    {
+    	showDialog(ownerFrame, hyperlinkListener, null, message, throwable);
+    }
+
+    /**
+     * Shows the error dialog and waits for the user to acknowledge the dialog.
+     * <p>
+     * If the JetS3t property <tt>gui.verboseErrorDialog</tt> is set to true, this dialog will 
+     * display detailed information about the root cause of the error (the throwable, if provided)
+     * </p>
+     *
+     * @param ownerDialog
+     * the dialog that will own this dialog
+     * @param hyperlinkListener
+     * the listener that will act on any hyperlink events triggered by the user clicking on HTTP links.
+     * @param message
+     * a general error message, which should ideally be somewhat user-friendly.
+     * @param throwable
+     * the underlying exception that caused the error. 
+     */
+    public static void showDialog(JDialog ownerDialog, HyperlinkActivatedListener hyperlinkListener,
+    		String message, Throwable throwable) 
+    {
+    	showDialog(ownerDialog, hyperlinkListener, null, message, throwable);
     }
     
 }
